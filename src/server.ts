fb3418ec01424592c7b7e5074015cca1613e156a
Shivam Kumar <shivam.kumarsingh2324@gmail.com>
2025-11-16 21:29:07 +0530

    last try

--- src/server.ts
+++ src/server.ts
@@ -278,7 +278,4 @@ app.post('/ytchatbot', async (req, res) => {
         });
     }
 });
-const PORT = process.env.PORT || 3005;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+app.listen(3001);
